d7d44fe987946bff6bb5020454219e518243b102
Akik4 <51128959+Akik4@users.noreply.github.com>
2023-10-27 08:58:32 +0200

    refactor: path

--- projet/utils/common.php
+++ projet/utils/common.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 define('ADMIN_MAIL', 'mail@gmail.com');
-define('PROJECT_FOLDER', '/projet/');
+define('PROJECT_FOLDER', '/SprintWeb/projet/');
 define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 session_start();
